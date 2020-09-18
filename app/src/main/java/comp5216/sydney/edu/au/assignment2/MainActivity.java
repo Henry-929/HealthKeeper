ece4b2adc9f8f4c94abda7787cc1ae9542a740b5
yfan2831 <yfan2831@uni.sydeny.edu.au>
2020-09-18 15:10:42 +1000

    UI design of logger, info collector, main view, add meal view, and report view

---
+++ app/src/main/java/comp5216/sydney/edu/au/assignment2/MainActivity.java
@@ -0,0 +1,14 @@
+package comp5216.sydney.edu.au.assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
