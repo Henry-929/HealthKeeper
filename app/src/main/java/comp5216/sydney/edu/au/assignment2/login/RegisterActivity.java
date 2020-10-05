8f6a3f8ddfadba67393296441d41a9ddbb8da06f
yfan2831 <yfan2831@uni.sydeny.edu.au>
2020-10-05 15:37:23 +1100

    Framework for login registration and password reset interface

--- app/src/main/java/comp5216/sydney/edu/au/assignment2/login/RegisterActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/assignment2/login/RegisterActivity.java
@@ -25,7 +25,7 @@ public class RegisterActivity extends AppCompatActivity {
         RegisterInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                //todo
+                //todo 
             }
         });
 
