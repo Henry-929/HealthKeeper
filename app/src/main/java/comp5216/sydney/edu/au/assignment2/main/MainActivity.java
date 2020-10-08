9f84a88a33f628533c6a149598b67010fb23d292
Henry-929 <770204042z@gmail.com>
2020-10-08 11:40:06 +0800

    demo

--- app/src/main/java/comp5216/sydney/edu/au/assignment2/main/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/assignment2/main/MainActivity.java
@@ -8,9 +8,6 @@ import comp5216.sydney.edu.au.assignment2.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    //测试git
-    int i = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
