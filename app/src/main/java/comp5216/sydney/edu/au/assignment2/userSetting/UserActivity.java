aef3456adf06c9215713f212a3c01cc6ed6d3571
Henry-929 <770204042z@gmail.com>
2020-10-22 22:11:40 +0800

    published log out function

--- app/src/main/java/comp5216/sydney/edu/au/assignment2/userSetting/UserActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/assignment2/userSetting/UserActivity.java
@@ -1,9 +1,12 @@
 package comp5216.sydney.edu.au.assignment2.userSetting;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -74,27 +77,38 @@ public class UserActivity extends AppCompatActivity {
                 }
             }
         });
-//
-//        final Button healthReportBtn = findViewById(R.id.btn_userSetting_healthReport);
-//        healthReportBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v){
-//                Intent intent = new Intent(UserActivity.this, ReportActivity.class);
-//                if (intent != null) {
-//                    UserActivity.this.startActivity(intent);
-//                }
-//            }
-//        });
+
         final Button logOutBtn = findViewById(R.id.btn_userSetting_logOut);
         logOutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-
+                Log.i("LogOutActivity", "Log out ");
                 //todo .. 弹窗给用户，提示信息"确定是否要登出？"
-                Intent intent = new Intent(UserActivity.this, LoginActivity.class);
-                if (intent != null) {
-                    UserActivity.this.startActivity(intent);
-                }
+
+                // Use the Builder class for convenient dialog construction
+                AlertDialog.Builder builder = new AlertDialog.Builder(UserActivity.this);
+                // 2. Chain together various setter methods to set the dialog characteristics
+                builder.setTitle(R.string.dialog_delete_title)
+                        .setMessage(R.string.dialog_delete_msg)
+                        // Add the buttons
+                        .setPositiveButton(R.string.delete, new
+                                DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Intent intent = new Intent(UserActivity.this, LoginActivity.class);
+                                        if (intent != null) {
+                                            UserActivity.this.startActivity(intent);
+                                        }
+                                    }
+                                })
+                        .setNegativeButton(R.string.cancel, new
+                                DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        // User cancelled the dialog
+                                        // Nothing happens
+                                    }
+                                });
+                // Create the AlertDialog object
+                builder.create().show();
             }
         });
 
