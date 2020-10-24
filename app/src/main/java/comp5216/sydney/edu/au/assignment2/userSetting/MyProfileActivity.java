aca6efa95a80202aeb919dcb1686bdb42eccca60
铁蛋 <hyip0613@uni.sydney.edu.au>
2020-10-24 14:12:54 +1100

    MyProfileActivity usernameChange_toDatabase& userInfoChange_toDatabase()

--- app/src/main/java/comp5216/sydney/edu/au/assignment2/userSetting/MyProfileActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/assignment2/userSetting/MyProfileActivity.java
@@ -174,7 +174,6 @@ public class MyProfileActivity extends AppCompatActivity {
     }
 
     public void usernameChange_toDatabase(){
-
         //获取userID
         uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
@@ -185,20 +184,22 @@ public class MyProfileActivity extends AppCompatActivity {
                         User user = snapshot.getValue(User.class);
 
                         if(user!=null){
-                            String newKey = databaseReference.child("Users").child(uid).push().getKey();
+                            //将birthday weight 写入database
 
-                            UserPost userPost = new UserPost(username);
+                            for(DataSnapshot d: snapshot.getChildren()) {
+                                //d.getKey()是userInfo的key
 
-                            databaseReference.child("Users").child(uid).child(newKey).setValue(userPost)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void aVoid) {
-                                            Toast.makeText(MyProfileActivity.this,"Username have saved in database successfully!",Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
+                                final String userInfo_Key = d.getKey();
+                                if (userInfo_Key.equals("username")) {
 
-                        }else{
-                            Toast.makeText(MyProfileActivity.this,"ERROR!!!",Toast.LENGTH_SHORT).show();
+                                    //将 gender 写入数据库
+                                    databaseReference.child("Users").child(uid)
+                                            .child("username").setValue(username);
+
+                                } else {
+                                    Toast.makeText(MyProfileActivity.this, "ERROR!!!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
                         }
                     }
                     @Override
@@ -210,83 +211,91 @@ public class MyProfileActivity extends AppCompatActivity {
     }
 
     public void userInfoChange_toDatabase(){
-
         //获取userID
         uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
 
         databaseReference.child("Users").child(uid)
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        final User user = snapshot.getValue(User.class);
+                        User user = snapshot.getValue(User.class);
 
                         if(user!=null){
-                            final String newKey = databaseReference.child("Users").child(uid).push().getKey();
+                            //将birthday weight 写入database
 
-                            UserInfo userInfo = new UserInfo(gender,age,height,weight);
+                            for(DataSnapshot d: snapshot.getChildren()){
+                                //d.getKey()是userInfo的key
 
-                            databaseReference.child("Users").child(uid).child(newKey).setValue(userInfo)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                final String userInfo_Key = d.getKey();
+                                if(!userInfo_Key.equals("userID") && !userInfo_Key.equals("username") && !userInfo_Key.equals("email") && !userInfo_Key.equals("password")&& !userInfo_Key.equals("confirm_password")&& !userInfo_Key.equals("security")&& !userInfo_Key.equals("notFirstTime")) {
+
+                                    //将 gender 写入数据库
+                                    databaseReference.child("Users").child(uid)
+                                            .child(userInfo_Key).child("gender").setValue(gender);
+
+                                    //将 birthday 写入数据库
+                                    databaseReference.child("Users").child(uid)
+                                            .child(userInfo_Key).child("height").setValue(height);
+
+                                    //将 weight 写入数据库
+                                    databaseReference.child("Users").child(uid)
+                                            .child(userInfo_Key).child("weight").setValue(weight);
+
+
+                                    //将 birthday 写入数据库
+                                    databaseReference.child("Users").child(uid)
+                                            .child(userInfo_Key).child("age").setValue(age);
+
+
+
+                                    //将 bmi写入数据库
+                                    //先获取 height- 再计算bmi- 再将bmi写入数据库
+                                    databaseReference.child("Users").child(uid)
+                                            .child(userInfo_Key).addListenerForSingleValueEvent(new ValueEventListener() {
                                         @Override
-                                        public void onSuccess(Void aVoid) {
-                                            Toast.makeText(MyProfileActivity.this,"gender, age, height, weight have saved in database successfully!",Toast.LENGTH_SHORT).show();
-//                                            Intent intent = new Intent(InfoActivity_1.this, InfoActivity_2.class);
-//                                            InfoActivity_1.this.startActivity(intent);
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            for(DataSnapshot d: dataSnapshot.getChildren()) {
+                                                //Toast.makeText(MainActivity.this,"嗷嗷"+dataSnapshot.getValue().toString(),Toast.LENGTH_SHORT).show();
 
-                                            //将 bmi写入数据库
-                                            //先获取 height- 再计算bmi- 再将bmi写入数据库
-                                            databaseReference.child("Users").child(uid)
-                                                    .child(newKey).addListenerForSingleValueEvent(new ValueEventListener() {
-                                                @Override
-                                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                                    for(DataSnapshot d: dataSnapshot.getChildren()) {
-                                                        //Toast.makeText(MainActivity.this,"嗷嗷"+dataSnapshot.getValue().toString(),Toast.LENGTH_SHORT).show();
+                                                String d_Key = d.getKey();
+                                                if (d_Key.equals("height")) {
+                                                    height = d.getValue().toString();
 
-                                                        String d_Key = d.getKey();
-                                                        if (d_Key.equals("height")) {
-                                                            height = d.getValue().toString();
+                                                    //计算bmi
+                                                    //将String转Double,并保留2位小数：
+                                                    DecimalFormat df = new DecimalFormat("0.00");
 
-                                                            //计算bmi
-                                                            //将String转Double,并保留2位小数：
-                                                            DecimalFormat df = new DecimalFormat("0.00");
+                                                    //将weight height的String转换为double
+                                                    double d_weight = Double.parseDouble(weight);
+                                                    weight = df.format(d_weight);
 
-                                                            //将weight height的String转换为double
-                                                            double d_weight = Double.parseDouble(weight);
-                                                            weight = df.format(d_weight);
+                                                    double d_height = Double.parseDouble(height);
+                                                    d_height = d_height/100;
 
-                                                            double d_height = Double.parseDouble(height);
-                                                            d_height = d_height/100;
+                                                    //计算BMI
+                                                    //BMI = (kg) / (m)x(m)
+                                                    double d_bmi = d_weight / (d_height*d_height);
+                                                    //bmi = Float.toString(f_bmi);
+                                                    //DecimalFormat decimalFormat= new  DecimalFormat( ".00" ); //构造方法的字符格式这里如果小数不足2位,会以0补足.
+                                                    bmi =df.format(d_bmi); //format 返回的是字符串
 
-                                                            //计算BMI
-                                                            //BMI = (kg) / (m)x(m)
-                                                            double d_bmi = d_weight / (d_height*d_height);
-                                                            //bmi = Float.toString(f_bmi);
-                                                            //DecimalFormat decimalFormat= new  DecimalFormat( ".00" ); //构造方法的字符格式这里如果小数不足2位,会以0补足.
-                                                            bmi =df.format(d_bmi); //format 返回的是字符串
+                                                    //将bmi写入数据库
+                                                    databaseReference.child("Users").child(uid)
+                                                            .child(userInfo_Key).child("bmi").setValue(bmi);
 
-                                                            //将bmi写入数据库
-                                                            databaseReference.child("Users").child(uid)
-                                                                    .child(newKey).child("bmi").setValue(bmi);
-
-
-                                                        }
-                                                    }
-                                                }
-
-                                                @Override
-                                                public void onCancelled(@NonNull DatabaseError error) {
 
                                                 }
-                                            });
-                                            //todo 保存信息，退出
-                                            Intent intent = new Intent(MyProfileActivity.this, UserActivity.class);
-                                            if (intent != null) {
-                                                MyProfileActivity.this.startActivity(intent);
                                             }
                                         }
-                                    });
 
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError error) {
+
+                                        }
+                                    });
+                                }
+
+                            }
                         }else{
                             Toast.makeText(MyProfileActivity.this,"ERROR!!!",Toast.LENGTH_SHORT).show();
                         }
