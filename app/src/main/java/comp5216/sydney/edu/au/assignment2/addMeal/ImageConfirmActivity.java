7429cb513f6969c7bd5c3296dcfa9ab4842cf19f
铁蛋 <hyip0613@uni.sydney.edu.au>
2020-10-29 23:55:00 +1100

    ImageConfirm UserFoodAdd_toDatabase

--- app/src/main/java/comp5216/sydney/edu/au/assignment2/addMeal/ImageConfirmActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/assignment2/addMeal/ImageConfirmActivity.java
@@ -14,23 +14,35 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import comp5216.sydney.edu.au.assignment2.R;
 import comp5216.sydney.edu.au.assignment2.main.MainActivity;
 
 public class ImageConfirmActivity extends Activity {
 
-    public String foodNameInChinse,addName,addQuantity,addCategory;
+    public static String uid;
+    DatabaseReference databaseReference;
+
+    public String foodNameInChinse,addFoodName,addFoodQuantity,addFoodCategory;
     public Bitmap foodImage;
 
     private Spinner categorySpinner;
     private ArrayAdapter<String> spinneradapter = null;
-    private static final String [] addFoodCategory ={"Breakfast","Lunch","Dinner","Other"};
+    private static final String [] FoodCategory ={"Breakfast","Lunch","Dinner","Other"};
 
     private ImageView foodImageDisplay;
-    private EditText foodNameDisplay,foodQuantityDisplay;
+    private EditText editTextFoodName,editTextFoodQuantity;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,11 +59,11 @@ public class ImageConfirmActivity extends Activity {
         Button addCustom = (Button)findViewById(R.id.btn_add_custom_food);
 
         foodImageDisplay= (ImageView)findViewById(R.id.add_food_display_photo);
-        foodNameDisplay = (EditText)findViewById(R.id.photo_add_food_name);
-        foodQuantityDisplay= (EditText)findViewById(R.id.photo_add_food_quantity);
+        editTextFoodName = (EditText)findViewById(R.id.photo_add_food_name);
+        editTextFoodQuantity= (EditText)findViewById(R.id.photo_add_food_quantity);
 
         categorySpinner = (Spinner)findViewById(R.id.photo_add_food_category);
-        spinneradapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,addFoodCategory);
+        spinneradapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,FoodCategory);
 
         categorySpinner.setAdapter(spinneradapter);
         categorySpinner.setVisibility(View.VISIBLE);//设置默认显示
@@ -60,7 +72,7 @@ public class ImageConfirmActivity extends Activity {
             public void onItemSelected(AdapterView<?> arg0, View arg1,
                                        int arg2, long arg3) {
                 // TODO Auto-generated method stub
-                addCategory = addFoodCategory[arg2];
+                addFoodCategory = FoodCategory[arg2];
             }
             @Override
             public void onNothingSelected(AdapterView<?> arg0) {
@@ -68,17 +80,11 @@ public class ImageConfirmActivity extends Activity {
 
             }
         });
-        foodNameDisplay.setText(foodNameInChinse);
+        editTextFoodName.setText(foodNameInChinse);
 
-        confirmBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(ImageConfirmActivity.this, FoodDisplayActivity.class);
-                if (intent != null) {
-                    ImageConfirmActivity.this.startActivity(intent);
-                }
-            }
-        });
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+
 
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -130,5 +136,71 @@ public class ImageConfirmActivity extends Activity {
                 }
             }
         });
+
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //将user-food 该用户输入的食物信息存入数据库
+                UserFoodAdd();
+                //UserFoodAdd_toDatabase();
+            }
+        });
     }
+
+    public void UserFoodAdd(){
+
+        addFoodName = editTextFoodName.getText().toString();
+        addFoodQuantity = editTextFoodQuantity.getText().toString();
+
+        //if 用户输入不为空
+        if(addFoodName.isEmpty()){
+            editTextFoodName.setError("Food Name is required");
+            editTextFoodName.requestFocus();
+            return;
+        }
+        if(addFoodQuantity.isEmpty()){
+            editTextFoodQuantity.setError("Food Quantity is required");
+            editTextFoodQuantity.requestFocus();
+            return;
+        }
+
+        //将user-food 该用户输入的食物信息存入数据库
+        UserFoodAdd_toDatabase();
+    }
+    public void UserFoodAdd_toDatabase(){
+
+        //获取userID
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        databaseReference.child("Users").child(uid)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        //每存放一个食物信息 就创建一个新节点
+                        String newKey = databaseReference.child("Users").child(uid).push().getKey();
+
+                        UsersFood usersFood = new UsersFood(addFoodName, addFoodQuantity, addFoodCategory);
+
+                        databaseReference.child("Users").child(uid).child(newKey).setValue(usersFood)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        //跳转到food display页面
+                                        Intent intent = new Intent(ImageConfirmActivity.this, FoodDisplayActivity.class);
+                                        startActivity(intent);
+                                    }
+                                });
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
+
+    }
+
+
 }
